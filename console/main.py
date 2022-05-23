57cba48c15e070a47d5718c06a7dc0d3aab10b13
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2022-05-24 01:04:22 +0530

    Update main.py

--- console/main.py
+++ console/main.py
@@ -1,59 +1,9 @@
-from os import system
+from console import console 
 
+console.clear()
 
-class console:
-    """
-    This class is just an implementation of javascript console in python
-    """
-
-    def __init__(self) -> None:
-        pass
-
-    @staticmethod
-    def log(val, *args, **kwargs) -> None:
-        """
-        Prints output to the console
-        """
-        print(val, *args, **kwargs)
-
-    @staticmethod
-    def warn(val, *args, **kwargs) -> None:
-        """
-        Prints warning string
-        """
-        print(f"\033[1;33m{val}\033[m", *args, **kwargs)
-
-    @staticmethod
-    def error(val, *args, **kwargs) -> None:
-        """
-        Prints error string
-        """
-        print(f"\033[1;31m{val}\033[m", *args, **kwargs)
-
-    @staticmethod
-    def info(val, *args, **kwargs) -> None:
-        """
-        Prints info string
-        """
-        print(f"\033[1;34m{val}\033[m", *args, **kwargs)
-
-    @staticmethod
-    def debug(val, *args, **kwargs) -> None:
-        """
-        Prints debug string
-        """
-        print(f"\033[1;30m{val}\033[m", *args, **kwargs)
-
-    @staticmethod
-    def status(val, *args, **kwargs) -> None:
-        """
-        Prints a  purple colored string
-        """
-        print(f"\033[1;35m{val}\033[m", *args, **kwargs)
-
-    @staticmethod
-    def clear() -> None:
-        """
-        Clears the console
-        """
-        system("cls")
+console.log("Hello world")
+console.warn("Warning")
+console.error("Error")
+console.debug("Debug")
+console.status("Status")

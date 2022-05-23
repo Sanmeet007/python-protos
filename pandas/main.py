f65344feb34e67fe51ff9f509e43b9f32c114713
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2022-05-24 00:56:38 +0530

    Update main.py

--- pandas/main.py
+++ pandas/main.py
@@ -1,1 +1,30 @@
+from pd_df import DataFrame
 
+data = DataFrame(
+    [
+        {
+            "id": 1,
+            "data": "Hello wolrdEnim sunt ex cupidatat occaecat eiusmod aute .",
+            "uuid": "ghjk3878j321134",
+            "color": "red",
+        },
+        {
+            "id": 2,
+            "data": "Ex aute adipisicing esse do excepteur dolore. ",
+            "uuid": "eihr8rhre8013nqfwe-9u",
+        },
+        {
+            "id": 3,
+            "data": "Elit consectetur nisi esse fugiat anim irure.",
+            "uuid": "b08349m320m=-081",
+        },
+        {
+            "id": 4,
+            "data": "Aute consectetur voluptate sit est anim culpa.",
+            "uuid": "pojetq39n06=24-081",
+        },
+    ]
+)
+
+
+print(data)

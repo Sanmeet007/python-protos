a5323193cba285c276f9fcbbd40f63f9d5511aca
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2022-05-26 21:22:21 +0530

    Update main.py

--- pandas/main.py
+++ pandas/main.py
@@ -3,10 +3,10 @@ from pd_df import DataFrame
 data = DataFrame(
     [
         {
+            "color": "red",
             "id": 1,
             "data": "Hello wolrdEnim sunt ex cupidatat occaecat eiusmod aute .",
             "uuid": "ghjk3878j321134",
-            "color": "red",
         },
         {
             "id": 2,
@@ -20,10 +20,10 @@ data = DataFrame(
         },
         {
             "id": 4,
-            "data": "Aute consectetur voluptate sit est anim culpa.",
             "uuid": "pojetq39n06=24-081",
+            "data": "Aute consectetur voluptate sit est anim culpa.",
         },
-    ]
-)
+    ] 
+    , ["uuid" , "id" , "data"] , indexed=True)
 
 print(data)
